569cf837f06b361df2cefd56a7e3ea94dcbc3a08
Johnson <johnssoft@ya.ru>
2025-06-03 13:41:02 +0500

    Another ASF compatibility attempt

--- Source/_harmony/adaptivestorage/SubPatch_AdaptiveStorage_AddITabToLegacyStorages_Postfix.cs
+++ Source/_harmony/adaptivestorage/SubPatch_AdaptiveStorage_AddITabToLegacyStorages_Postfix.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using DigitalStorageUnit.compat;
 using Verse;
 
@@ -10,9 +11,14 @@ public static class SubPatch_AdaptiveStorage_AddITabToLegacyStorages_Postfix
     /// <summary>
     ///     Removes the Adaptive Storage Framework patch for our buildings, that adds "Content" inspection tab for Building_Storage things.
     /// </summary>
-    public static bool Prefix(object[] __args)
+    public static bool Prefix(object[] __args, ref IEnumerable<InspectTabBase>? __result)
     {
-        if (__args[1] is Thing and (IRemoveStorageInspectionTab or DigitalStorageUnitBuilding)) return false;
+        if (__args[1] is Thing and (IRemoveStorageInspectionTab or DigitalStorageUnitBuilding))
+        {
+            __result = __args[0] as IEnumerable<InspectTabBase>;
+            return false;
+        }
+
         return true;
     }
 }
